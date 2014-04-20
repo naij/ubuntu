8b60dc50e2395bb3b1ffa15f7fb0d0c21a723d93
陆议 <fruit.wj@taobao.com>
2014-04-20 23:17:44 +0800

    node 安装更新

--- softwares/nodejs/install-nodejs.sh
+++ softwares/nodejs/install-nodejs.sh
@@ -2,27 +2,25 @@
 
 echo "nodejs 开始安装"
 
-sudo apt-get install python
-sudo apt-get install build-essential
+#sudo apt-get install python
+#sudo apt-get install build-essential
 
-wget http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz
+#wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
 ​
-tar -xvf node-v0.10.26-linux-x64.tar.gz
+#tar -xvf node-v0.10.26.tar.gz
 
-cd node-v0.10.26-linux-x64
+cd node-v0.10.26
 
-chmod +x configure
+#chmod +x configure
 
-./configure --prefix=/usr/local/node
+#./configure --prefix=/usr/local/node
 
-make
+#make
 
-sudo make install
+#sudo make install
 
-sudo ln -s /usr/local/node/bin/node  /usr/bin/node 
-sudo ln -s /usr/local/node/lib/node  /usr/lib/node 
-sudo ln -s /usr/local/node/bin/npm  /usr/bin/npm
+cd ../
 
-rm -rf node-v0.10.26-linux-x64*
+rm -rf node-v0.10.26*
 
 echo "nodejs 安装完毕"
