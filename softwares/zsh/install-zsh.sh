9390d28089fb2c1be12adab5661366ac1b1d3094
陆议 <fruit.wj@taobao.com>
2014-04-21 10:43:26 +0800

    zsh 更新

--- softwares/zsh/install-zsh.sh
+++ softwares/zsh/install-zsh.sh
@@ -3,7 +3,7 @@
 echo "zsh 开始安装"
 
 sudo apt-get install curl zsh
-chsh -s /bin/zsh
+chsh -s /bin/zsh wangjian
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 path=`dirname $0`
 cp $path/.zshrc ~
